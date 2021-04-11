5178b48bc52485fb14204850a2d6cae1ca1636c8
Douglas Cardoso <douglascf12@live.com>
2021-04-11 15:07:30 -0300

    Initial Commit

---
+++ Increment/IncrementApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncrementApp.swift
+//  Increment
+//
+//  Created by Douglas Cardoso Ferreira on 11/04/21.
+//
+
+import SwiftUI
+
+@main
+struct IncrementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
