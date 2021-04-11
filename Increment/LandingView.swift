673dff69b6fe13c886d4333065365e56fae1fa6d
Douglas Cardoso <douglascf12@live.com>
2021-04-11 15:19:31 -0300

    first commit

--- Increment/LandingView.swift
+++ Increment/LandingView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LandingView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LandingView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LandingView()
     }
 }
