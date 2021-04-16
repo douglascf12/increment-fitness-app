6503451ed1742c2083e2b5d1a14d8b6b1d91f743
Douglas Cardoso <douglascf12@live.com>
2021-04-16 12:07:58 -0300

    video class part 3 finished

--- Increment/Color+Extension.swift
+++ Increment/Color+Extension.swift
@@ -9,4 +9,5 @@ import SwiftUI
 
 extension Color {
     static let darkPrimaryButton = Color(red: 106/255, green: 167/255, blue: 194/255, opacity: 0.3)
+    static let primaryButton = Color("primaryButton")
 }
--- Increment/LandingView.swift
+++ Increment/LandingView.swift
@@ -8,41 +8,48 @@
 import SwiftUI
 
 struct LandingView: View {
+    @State private var isActive = false
     var body: some View {
-        GeometryReader { proxy in
-            VStack {
-                Spacer().frame(height: proxy.size.height * 0.18)
-                Text("Increment")
-                    .font(.system(size: 64, weight: .medium))
-                    .foregroundColor(.white)
-                Spacer()
-                Button(action: {}) {
-                    HStack(spacing: 15) {
-                        Spacer()
-                        Image(systemName: "plus.circle")
-                            .font(.system(size: 24, weight: .semibold))
-                            .foregroundColor(.white)
-                        Text("Create a challenge")
-                            .font(.system(size: 24, weight: .semibold))
-                            .foregroundColor(.white)
-                        Spacer()
+        NavigationView {
+            GeometryReader { proxy in
+                VStack {
+                    Spacer().frame(height: proxy.size.height * 0.18)
+                    Text("Increment")
+                        .font(.system(size: 64, weight: .medium))
+                        .foregroundColor(.white)
+                    Spacer()
+                    NavigationLink(destination: CreateView(), isActive: $isActive) {
+                        Button(action: {
+                            isActive = true
+                        }) {
+                            HStack(spacing: 15) {
+                                Spacer()
+                                Image(systemName: "plus.circle")
+                                    .font(.system(size: 24, weight: .semibold))
+                                    .foregroundColor(.white)
+                                Text("Create a challenge")
+                                    .font(.system(size: 24, weight: .semibold))
+                                    .foregroundColor(.white)
+                                Spacer()
+                            }
+                        }.padding(15)
+                        .buttonStyle(PrimaryButtonStyle())
                     }
-                }.padding(.horizontal, 15)
-                .buttonStyle(PrimaryButtonStyle())
-            }.frame(
-                maxWidth: .infinity,
-                maxHeight: .infinity
-            )
-            .background(
-                Image("pullups")
-                    .resizable()
-                    .aspectRatio(
-                        contentMode: .fill
-                    ).overlay(Color.black.opacity(0.4))
-                    .frame(width: proxy.size.width)
-                    .edgesIgnoringSafeArea(.all)
-            )
-        }
+                }.frame(
+                    maxWidth: .infinity,
+                    maxHeight: .infinity
+                )
+                .background(
+                    Image("pullups")
+                        .resizable()
+                        .aspectRatio(
+                            contentMode: .fill
+                        ).overlay(Color.black.opacity(0.4))
+                        .frame(width: proxy.size.width)
+                        .edgesIgnoringSafeArea(.all)
+                )
+            }
+        }.accentColor(.primary)
     }
 }
 
